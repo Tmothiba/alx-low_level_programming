19eb7b84dad5bdfdbdd17c295c8cfe33eefc8d17
Tmothiba <mothibatshwarelo99@gmail.com>
2023-08-12 20:06:18 -0700

    lower case alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - printing last didit of a number
+ *
+ * Return: Always 0 (Succes)
+ */
+int main()
+{
+        char albet = 'a';
+        for( albet = 'a'; albet <= 'z';albet++)
+        {
+                putchar(albet);
+        }
+        putchar('\n');
+        return(0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - printing last didit of a number
+ *
+ * Return: Always 0 (Succes)
+ */
+int main()
+{	
+	int alphabet = 97;
+	int alphabet2 = 65;
+	while( alphabet <= 122)
+	{
+		putchar(alphabet);
+	}	
+	 while (alphabet2 <= 90)
+	 {
+		 putchar(alphabet2);
+	 }
+
+	putchar('\n');
+	return(0);
+}
