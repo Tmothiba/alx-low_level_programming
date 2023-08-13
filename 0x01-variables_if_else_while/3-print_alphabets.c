dc10ac5c6e5a92255f10a7e4a8f72d440be3c0ff
Tmothiba <mothibatshwarelo99@gmail.com>
2023-08-12 21:04:36 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,19 +4,18 @@
  *
  * Return: Always 0 (Succes)
  */
-int main()
-{	
-	int alphabet = 97;
-	int alphabet2 = 65;
-	while( alphabet <= 122)
-	{
-		putchar(alphabet);
-	}	
-	 while (alphabet2 <= 90)
-	 {
-		 putchar(alphabet2);
-	 }
+int main(void)
+{
+	char albet1, albet2;
 
+	for (albet1 = 'a'; albet1 <= 'z'; albet1++)
+	{
+		putchar(albet1);
+	}
+	for (albet2 = 'A'; albet2 <= 'Z'; albet2++)
+	{
+		putchar(albet2);
+	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
