f507a95e8e816899b047616ebab54201196e7f92
Tmothiba <mothibatshwarelo99@gmail.com>
2023-08-12 23:41:19 -0700

    printing combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,9 +9,13 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		putchar('0' + num);
+		if (num == 9)
+		{
+			break;
+		}
 		putchar(',');
 		putchar('\t');
 	}
