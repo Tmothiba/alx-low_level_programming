4f4b95ca5c28e9794fd19174922df5a785bcf560
Tmothiba <mothibatshwarelo99@gmail.com>
2023-08-18 17:00:05 -0700

    adding numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add- adding two numbers
+ * @a: number to be added
+ * @b: number to be added
+ * Return: added output
+ */
+int add(int a, int b)
+{
+	int added;
+
+	added = a + b;
+	return (added);
+}
