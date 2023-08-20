edff472788224238595c99df4f7306b0329650c0
Tmothiba <mothibatshwarelo99@gmail.com>
2023-08-19 18:21:58 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -15,10 +15,10 @@ void print_line(int n)
 	}
 	else
 	{
-		for (line = 1; line <= n; line++)
+		for (line = 0; line <= n; line++)
 		{
 			_putchar('_');
-			_putchar('\n');
 		}
+			_putchar('\n');
 	}
 }
