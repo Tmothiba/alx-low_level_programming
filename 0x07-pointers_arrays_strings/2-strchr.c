a680a2631332ab945e46d04148339c4d65f541a1
Tmothiba <mothibatshwarelo99@gmail.com>
2023-08-29 19:56:08 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strchr- locates a character in a string
+ * @s: string to be scanned
+ * @c: character to be searched for in s
+ * Return: char
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+	if (*s != c)
+	{
+		s++;
+	}
+	else
+	{
+		return (s);
+	}
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
+}
