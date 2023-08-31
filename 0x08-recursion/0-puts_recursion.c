50a2d3d227f7dbdf72b021b3b0d788d675215456
Tmothiba <mothibatshwarelo99@gmail.com>
2023-08-31 12:13:22 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,6 +12,6 @@ void _puts_recursion(char *s)
 		puts(" ");
 		return;
 	}
-	putchar(*s);
+	_putchar(*s);
 	_puts_recursion(s + 1);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,5 +6,5 @@ void _puts_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int is_prime_number(int n);
-
+int _putchar(char c);
 #endif
