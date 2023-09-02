33c2a60529009e7676ccbc395565b5f2dc333b8c
Tmothiba <mothibatshwarelo99@gmail.com>
2023-09-02 07:40:33 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -22,14 +22,13 @@ void puts_half(char *str)
 			_putchar(str[i]);
 		}
 	}
-	else if ( len % 2 != 0)
+	else if (len % 2 != 0)
 	{
 		for (n = (len - 1) / 2; n < (len - 1); n++)
 		{
 			_putchar(str[n + 1]);
 		}
 	}
-	  
 	_putchar('\n');
-} 
 
+}
