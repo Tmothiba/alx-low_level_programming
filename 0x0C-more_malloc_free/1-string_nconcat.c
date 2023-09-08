63c6b78686881585655166c441c9cd86bbb49f04
Tmothiba <mothibatshwarelo99@gmail.com>
2023-09-08 13:29:34 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = lenS2;
 	}
 
-	ptr = (char *)malloc(sizeof(char) * (lenS1 + lenS2 + 1));
+	ptr = (char *)malloc(sizeof(char) * (lenS1 + n + 1));
 	if (ptr == NULL)
 	{
 		return (NULL);
