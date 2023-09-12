2c34187c856c1c1ce0d3c1be1b4ff49518ddaf81
Tmothiba <mothibatshwarelo99@gmail.com>
2023-09-12 13:51:53 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -58,10 +58,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (!nameCopy && name)
 	{
 		free(new);
-		return(NULL);
+		return (NULL);
 	}
 	ownerCopy = newstr(owner);
-	if(!ownerCopy && owner)
+	if (!ownerCopy && owner)
 	{
 		free(nameCopy);
 		free(new);
