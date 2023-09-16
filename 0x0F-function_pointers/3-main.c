c7b562916b32137d8417e40be68f7e38e834c718
Tmothiba <mothibatshwarelo99@gmail.com>
2023-09-15 20:59:03 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+/**
+ * main - main program
+ * @argc: number of command line argumants
+ * @argv: array containing arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int results;
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+	op = get_op_func(argv[2]);
+		if (!op)
+		{
+			printf("Error\n");
+			exit (99);
+		}
+		if ((argv[3][0] == '0' && argv[2][0] == '/')||argv[2][0] == '%')
+		{
+			printf("Error\n");
+			exit (100);
+		}
+		results = op(atoi(argv[1]), atoi(argv[3]));
+		printf("%d\n", results);
+		return (0);
+}
+
+
+
+
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+/**
+ * op_add - Function that adds numbers
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return(a + b);
+}
+/**
+ * op_sub - function that substract numbers
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a -b);
+}
+/**
+ * op_mul- function that multiplies numbers
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function that divides numbers
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function that checks the moduli of a number
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+
