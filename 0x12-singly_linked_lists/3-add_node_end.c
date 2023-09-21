51597e64b9dab1a0a11f560edf8ddae08da872ce
Tmothiba <mothibatshwarelo99@gmail.ccom>
2023-09-21 13:04:45 -0700

    adds a new node at the end of a list_t list.

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,7 +9,7 @@
  * Return: address of new element or NULL if fails
  *
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	list_t *temp = *head;
